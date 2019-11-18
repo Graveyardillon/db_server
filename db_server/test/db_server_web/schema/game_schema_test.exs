7118b547abfbd9e06606ff320a64940644a46adf
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-11-19 00:45:17 +0900

    テスト追加

--- db_server/test/db_server_web/schema/game_schema_test.exs
+++ db_server/test/db_server_web/schema/game_schema_test.exs
@@ -9,7 +9,35 @@ defmodule DbServerWeb.GameSchemaTest do
     @valid_params %{game_name: "test_name"}
     @invalid_params %{game_name: nil}
 
-  test "insertion" do
-    assert {:ok, %Game{} = game} = Games.create_game(@valid_params)
+    test "insertion with valid data" do
+      assert {:ok, %Game{} = game} = Games.create_game(@valid_params)
+      assert game.game_name == "test_name"
+    end
+
+    test "insertion with invalid data" do
+      assert {:error, %Ecto.Changeset{}} = Games.create_game(@invalid_params)
+    end
+  end
+
+  describe "deletion" do
+    @valid_params %{game_name: "tset_name"}
+
+    test "deletion with valid data" do
+      assert {:ok, %Game{} = game} = Games.delete_game(@valid_params)
+    end
+
+    test "deletion with invalid data" do
+    
+    end
+  end
+
+  describe "update" do
+    test "update with valid data" do
+    
+    end
+
+    test "update with invalid data" do
+      
+    end
   end
 end
