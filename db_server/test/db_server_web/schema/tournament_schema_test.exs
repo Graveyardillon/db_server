6a8a7e1517433ff82ed644469b3ccf868b003001
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-11-21 18:55:06 +0900

    invalid test worked

--- db_server/test/db_server_web/schema/tournament_schema_test.exs
+++ db_server/test/db_server_web/schema/tournament_schema_test.exs
@@ -19,11 +19,30 @@ defmodule DbServerWeb.TournamentSchemaTest do
       tournament_name: "updated_one"
     }
 
+    @invalid_insert_params %{
+      tournament_name: nil,
+      tournament_duration: 120,
+      tournament_participation_deadline: DateTime.utc_now(),
+      team_number_limit: 2,
+      player_number_limit: 2
+    }
+
+    @invalid_update_params %{
+      tournament_duration: nil
+    }
+
     test "crud test." do
       assert {_, struct} = Tournaments.create_tournament(@insert_params)
       assert %Tournament{} = tournament = Tournaments.get_tournament(struct.id)
       assert {:ok, %Tournament{} = tournament} = Tournaments.update_tournament(tournament, @update_params)
       assert {:ok, %Tournament{} = tournament} = Tournaments.delete_tournament(tournament)
+    end
+
+    test "invalid cru test." do
+      assert {:error, struct} = Tournaments.create_tournament(@invalid_insert_params)
+      assert {_, struct} = Tournaments.create_tournament(@insert_params)
+      assert %Tournament{} = tournament = Tournaments.get_tournament(struct.id)
+      assert {:error, %Ecto.Changeset{} = tournament} = Tournaments.update_tournament(tournament, @invalid_update_params)
     end
   end
 end
