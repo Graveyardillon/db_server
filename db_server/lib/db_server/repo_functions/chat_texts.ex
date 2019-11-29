6535b872eb72ca2675618a74d01fb6d63b85eb52
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-11-29 14:26:51 +0900

    チャットのdbのファイルだけ作成

---
+++ db_server/lib/db_server/repo_functions/chat_texts.ex
@@ -0,0 +1,6 @@
+defmodule DbServer.RepoFunctions.ChatTexts do
+  @moduledoc """
+    The chat texts context.
+  """
+  use DbServer.AroundRepo
+end
---
+++ db_server/test/db_server_web/schema/chat_text_schema_test.exs
@@ -0,0 +1,11 @@
+defmodule DbServerWeb.ChatTextSchemaTest do
+  use ExUnit.Case, async: true
+
+  use DbServer.DataCase
+  use DbServer.AroundRepo
+
+  # Chat text uses 2 many_to_many relationships.
+  describe "chat text schema test." do
+    
+  end
+end
