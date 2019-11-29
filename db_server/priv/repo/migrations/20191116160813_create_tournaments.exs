b2de7c20dff539589a3f845002be2ef7318f6017
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-11-29 14:17:32 +0900

    null制約の微修正

--- db_server/priv/repo/migrations/20191116160813_create_tournaments.exs
+++ db_server/priv/repo/migrations/20191116160813_create_tournaments.exs
@@ -4,7 +4,7 @@ defmodule DbServer.Repo.Migrations.CreateTournaments do
   def change do
     create table(:tournaments) do
       add :name, :string, null: false
-      add :game_id, references(:games), null: false
+      add :game_id, references(:games)#TODO:, null: false
       add :duration, :integer, null: false
       add :participation_deadline, :utc_datetime, null: false
       add :user_id, references(:users, type: :string, column: :id)
