eba37a1f87fd4db03ecaf177f6c47cb0bae65fd9
Papillon6814 <so1rou.k@gmail.com>
2019-12-08 22:21:43 +0900

    controller test worked green

--- db_server/test/db_server_web/controllers/game_controller_test.exs
+++ db_server/test/db_server_web/controllers/game_controller_test.exs
@@ -13,10 +13,7 @@ defmodule DbServerWeb.GameControllerTest do
   test "create game", set do
     {:ok, result} = set.game
 
-    conn = 
-      build_conn()
-      |> post("game/create", result |> Map.from_struct())
-      |> json_response(200)
+    assert conn = build_conn(:post, "/api/create", result)
       
     #assert conn.status == 204
   end
